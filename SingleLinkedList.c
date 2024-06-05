5c76e8a32bb3bc3fb64945652f2544f41f5ff61d
Tejas Shah <tejasshah2k19@gmail.com>
2024-06-05 15:35:29 +0530

    add beg single linked list

--- SingleLinkedList.c
+++ SingleLinkedList.c
@@ -39,25 +39,38 @@ void display(){
             p = p->next; 
         }
 }
+
+void addBeg(){
+    struct node *p; 
+
+    p = malloc(sizeof(struct node)); 
+    printf("\nEnter name and rollNUm");
+    scanf("%s%d",&p->name,&p->rollNum);
+    p->next = head ; 
+    head = p; 
+}
 int main(){
 
     int choice;
 
     while(1){ 
-    printf("\n0 for exit\n1 For Add Student\n2 for List\nEnter choice");
+    printf("\n0 for exit\n1 For Add Student\n2 for List\n3 for Add Beg\nEnter choice");
     scanf("%d",&choice);
 
 
         switch (choice)
         {
         case 1:
-            addNode();
+            addNode();//last 
             break;
         case 2:
             display();
             break;    
         case 0:
             exit(0);
+        case 3:
+            addBeg();
+            break;
         
         default:
             break;
